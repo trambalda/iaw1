67d30cf09cbd513dc417d2d0659f3e50c22b3235
dinarm <damukhlisov@gmail.com>
2025-03-13 14:47:55 +0300

    Verify scene added

---
+++ iaw01/Scenes/DummyScene/VerifyPhoneNumberScene/VerifyPhoneNumberViewController.swift
@@ -0,0 +1,16 @@
+//
+//  VerifyPhoneNumberViewController.swift
+//  iaw01
+//
+//  Created by Dinar Mukhlisov on 13.03.2025.
+//
+
+import UIKit
+
+class VerifyPhoneNumberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
