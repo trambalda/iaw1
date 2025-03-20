961711337fa64c4e6dde69c93ba8fb702b0eed76
AliFragile <alinka.razheva@mail.ru>
2025-03-20 13:52:51 +0300

    fixed code after review 5

--- iaw01/Components/TableViewCells/DishTableViewCell/DishCellModel.swift
+++ iaw01/Components/TableViewCells/DishTableViewCell/DishCellModel.swift
@@ -1,10 +1,11 @@
 import UIKit
 
 struct DishCellModel {
-    static var empty = DishCellModel(foodImage: nil, foodTitle: "", restaurantImage: nil, restaurantTitle: "")
     let foodImage: UIImage?
     let foodTitle: String
     let restaurantImage: UIImage?
     let restaurantTitle: String
+    
+    static var empty = DishCellModel(foodImage: nil, foodTitle: "", restaurantImage: nil, restaurantTitle: "")
 }
 
--- iaw01/Components/TableViewCells/RestaurantTableViewCell/RestaurantCellModel.swift
+++ iaw01/Components/TableViewCells/RestaurantTableViewCell/RestaurantCellModel.swift
@@ -1,8 +1,9 @@
 import UIKit
 
 struct RestaurantCellModel {
-    static var empty = RestaurantCellModel(image: nil, title: "", address: "") 
     let image: UIImage?
     let title: String
     let address: String
+    
+    static var empty = RestaurantCellModel(image: nil, title: "", address: "") 
 }
