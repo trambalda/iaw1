6a29ff14e34c250c19fe6e37d78302ac09082b78
Akatova Daria <dariaakatova810@gmail.com>
2025-03-23 21:24:54 +0300

    Доработка компоненты AuthorizationSegmentedControl

--- iaw01/Scenes/AuthorizationScene/AuthorizationSegmentedControl.swift
+++ iaw01/Scenes/AuthorizationScene/AuthorizationSegmentedControl.swift
@@ -9,52 +9,62 @@ import UIKit
 
 class AuthorizationSegmentedControl: UIView {
     
+    private enum Constants {
+        static let containerHeight: CGFloat = 63
+        static let buttonHeight: CGFloat = 43
+        static let containerCornerRadius: CGFloat = containerHeight / 2
+        static let buttonCornerRadius: CGFloat = buttonHeight / 2
+    }
+    
     private  func createButton(title: String) -> UIButton {
         let button = UIButton(type: .system)
         button.backgroundColor = .pink100
         button.setTitle(title, for: .normal)
         button.setTitleColor(.light100, for: .normal)
-        button.layer.cornerRadius = 43 / 2
+        button.layer.cornerRadius = Constants.buttonCornerRadius
         return button
     }
-  
+    
     private lazy var loginButton: UIButton = createButton(title: "Login")
     private lazy var signUpButton: UIButton = createButton(title: "Sign Up")
-    
     
     private let containerView: UIView = {
         let view = UIView()
         view.backgroundColor = .pink60
-//        Если высота 63 (или выше, кнопки 43) фигурирует в констрейнтах, то есть упоминается в разных местах класса, удобнее вынести это в константы.
-        view.layer.cornerRadius = 63 / 2
+        view.layer.cornerRadius = Constants.containerCornerRadius
+        view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
     private let stackView: UIStackView = {
-        let stackViewInContainer = UIStackView()
-//        Дефолтное значение не нужно указывать
-        stackViewInContainer.axis = .horizontal
-        stackViewInContainer.spacing = 10
-        stackViewInContainer.distribution = .fillEqually
-        stackViewInContainer.alignment = .center
-        return stackViewInContainer
+        let stackView = UIStackView()
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
     }()
     
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        configure()
+    }
+    
+    private func configure() {
         setupLayout()
         setupConstraints()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-
     private func setupLayout() {
         addSubview(containerView)
         containerView.addSubview(stackView)
+        addSubview(stackView)
         stackView.addArrangedSubview(loginButton)
         stackView.addArrangedSubview(signUpButton)
     }
@@ -63,25 +73,21 @@ class AuthorizationSegmentedControl: UIView {
 extension AuthorizationSegmentedControl {
     private func setupConstraints() {
         
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
         NSLayoutConstraint.activate([
             
-//            нужно установить topAnchor.constraint(equalTo: stackViewLabels.bottomAnchor, constant: 21), не знаю, как реализовать
-            containerView.topAnchor.constraint(equalTo: topAnchor, constant: 137),
+            containerView.topAnchor.constraint(equalTo: topAnchor, constant: 21),
             containerView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             containerView.widthAnchor.constraint(equalToConstant: 358),
-            containerView.heightAnchor.constraint(equalToConstant: 63),
+            containerView.heightAnchor.constraint(equalToConstant: Constants.containerHeight),
             
             stackView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
             stackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
             stackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
-            stackView.heightAnchor.constraint(equalToConstant: 43),
+            stackView.heightAnchor.constraint(equalToConstant: Constants.buttonHeight),
             
-            loginButton.heightAnchor.constraint(equalToConstant: 43),
-            signUpButton.heightAnchor.constraint(equalToConstant: 43),
+            loginButton.heightAnchor.constraint(equalToConstant: Constants.buttonHeight),
+            signUpButton.heightAnchor.constraint(equalToConstant: Constants.buttonHeight)
         ])
     }
 }
--- iaw01/Scenes/AuthorizationScene/AuthorizationTitleView.swift
+++ iaw01/Scenes/AuthorizationScene/AuthorizationTitleView.swift
@@ -10,29 +10,38 @@ import UIKit
 class AuthorizationTitleView: UIStackView {
     
     private let titleLabel: UILabel = {
-         let label = UILabel()
-         label.attributedText = Font.heading4.compose("Welcome", color: .dark100)
-         return label
+        let label = UILabel()
+        label.attributedText = Font.heading4.compose("Welcome", color: .dark100)
+        return label
     }()
     
     private let descriptionLabel: UILabel = {
-         let label = UILabel()
+        let label = UILabel()
         label.attributedText = Font.body.compose("Sign up or Login to your Account", color: .dark80)
-         return label
+        return label
     }()
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupLayout()
+        configure()
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+        configure()
+    }
+    
+    private func configure() {
+        setupStackView()
+        setupLayout()
+    }
+    
+    private func setupStackView() {
+        axis = .vertical
+        spacing = -3
     }
     
     private func setupLayout() {
-        axis = .vertical
-        spacing = -3
         addArrangedSubview(titleLabel)
         addArrangedSubview(descriptionLabel)
     }
