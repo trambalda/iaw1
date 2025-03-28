1b6a5f00fc1e15a09f347fac340347b7e747a7ba
Evgenii Kirillov <477504@gmail.com>
2025-03-28 14:01:30 +0400

    fix: del init

--- iaw01/Scenes/OnboardingScene/OnboardingViewController.swift
+++ iaw01/Scenes/OnboardingScene/OnboardingViewController.swift
@@ -19,10 +19,6 @@ final class OnboardingViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func loadView() {
         view = onboardingView
     }
