4d13a99e4c5e6f1794d37fb0f975c7acc0084a2f
AliFragile <alinka.razheva@mail.ru>
2025-04-03 11:54:20 +0300

    changed constraints

--- iaw01/Scenes/RestaurantScene/RestaurantView.swift
+++ iaw01/Scenes/RestaurantScene/RestaurantView.swift
@@ -43,8 +43,12 @@ final class RestaurantView: UIView {
     
     func setupLayoutAndConstraints() {
         backgroundColor = .light100
+        
+        let headerWrapperStack = UIStackView(arrangedSubviews: [headerImageView])
+        headerWrapperStack.axis = .vertical
+        headerWrapperStack.translatesAutoresizingMaskIntoConstraints = false
        
-        let headerStack = UIStackView(arrangedSubviews: [headerImageView, restaurantHeaderView, restaurantInfoView])
+        let headerStack = UIStackView(arrangedSubviews: [restaurantHeaderView, restaurantInfoView])
         headerStack.axis = .vertical
         headerStack.spacing = 22
         headerStack.alignment = .center
@@ -64,6 +68,7 @@ final class RestaurantView: UIView {
         scrollView.showsVerticalScrollIndicator = false
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(scrollView)
+        scrollView.addSubview(headerWrapperStack)
         scrollView.addSubview(mainStack)
         
         headerImageViewHeightConstraint = headerImageView.heightAnchor.constraint(equalToConstant: 164)
@@ -73,18 +78,18 @@ final class RestaurantView: UIView {
             scrollView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
             scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
-           
+            scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
             
-            mainStack.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            headerWrapperStack.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            headerWrapperStack.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            headerWrapperStack.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+           
+            mainStack.topAnchor.constraint(equalTo: headerWrapperStack.bottomAnchor, constant: 18),
             mainStack.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 21),
             mainStack.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -21),
             mainStack.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
             mainStack.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -21*2),
             mainStack.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor),
-   
-            ///headerImageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            ///headerImageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            ///headerImageView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
         ])
     }
     
@@ -95,51 +100,3 @@ final class RestaurantView: UIView {
         }
     }
 }
-/*
- func setupLayoutAndConstraints() {
-         backgroundColor = .light100
-         
-         let headerStack = UIStackView(arrangedSubviews: [restaurantHeaderView, restaurantInfoView])
-         headerStack.axis = .vertical
-         headerStack.spacing = 22
-         headerStack.alignment = .center
-         headerStack.setCustomSpacing(18, after: restaurantInfoView)
-         
-         let filtersStack = UIStackView(arrangedSubviews: [menuTimeView, menuCategoryView])
-         filtersStack.axis = .vertical
-         filtersStack.spacing = 18
-         filtersStack.alignment = .center
-         filtersStack.translatesAutoresizingMaskIntoConstraints = false
-         filtersStack.setCustomSpacing(9, after: menuCategoryView)
-         
-         scrollView.showsVerticalScrollIndicator = false
-         scrollView.translatesAutoresizingMaskIntoConstraints = false
-         addSubview(scrollView)
-         scrollView.addSubview(headerImageView)
-         scrollView.addSubview(headerStack)
-         scrollView.addSubview(filtersStack)
-         
-         headerImageViewHeightConstraint = headerImageView.heightAnchor.constraint(equalToConstant: 164)
-         restaurantInfoViewHeightConstraint = restaurantInfoView.heightAnchor.constraint(equalToConstant: 109)
-         
-         NSLayoutConstraint.activate([
-             scrollView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
-             scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
-             scrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
-             
-             headerImageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-             headerImageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-             headerImageView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-             headerImageView.heightAnchor.constraint(equalToConstant: 164),
-             
-             headerStack.topAnchor.constraint(equalTo: headerImageView.bottomAnchor),
-             headerStack.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 21),
-             headerStack.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -21),
-             ///headerStack.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-             
-             filtersStack.topAnchor.constraint(equalTo: headerStack.bottomAnchor),
-             filtersStack.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-             filtersStack.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor)
-         ])
-     }
- */
