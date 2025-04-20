59eb1290bbf24eda96d8d2a47ae48e40982b10a5
alex <hordiah1@google.com>
2025-04-20 15:27:33 +0300

    rename methods coordinator from KeyboardScene

--- iaw01/Scenes/HomeScenesCoordinator.swift
+++ iaw01/Scenes/HomeScenesCoordinator.swift
@@ -4,7 +4,7 @@ protocol HomeScenesCoordinatorProtocol {
     func showTextFieldScene()
     func showCornersButtonsScene()
     func showVerifyPhoneNumberScene()
-    func showKeyboradSrviceScene()
+    func showKeyboarddServiceScene()
 }
 
 final class HomeScenesCoordinator: CoordinatorProtocol, HomeScenesCoordinatorProtocol {
@@ -34,7 +34,7 @@ final class HomeScenesCoordinator: CoordinatorProtocol, HomeScenesCoordinatorPro
         rootViewController.pushViewController(vc, animated: true)
     }
 
-    func showKeyboradSrviceScene() {
+    func showKeyboarddServiceScene() {
         let vc = factory.createKeyboardServiceScene()
         rootViewController.pushViewController(vc, animated: true)
     }
