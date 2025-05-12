ee0337d7b21de4075783d271ac797740e719318e
alex <hordiah1@google.com>
2025-05-12 19:13:53 +0300

    fix return property

--- iaw01/Scenes/HomeScenes/HomeScenesFactory.swift
+++ iaw01/Scenes/HomeScenes/HomeScenesFactory.swift
@@ -30,6 +30,7 @@ final class HomeScenesFactory: Factory {
     func createKeyboardServiceScene() -> KeyboardServiceViewController {
         let vc = KeyboardServiceViewController()
         keyboardService.isEnabled = true
+        return vc
     }
 
     func createAuthorizationScene() -> AuthorizationViewController {
