e869f337b9648a598919b79184b05ab7903dfeb5
Akatova Daria <dariaakatova810@gmail.com>
2025-05-22 20:16:25 +0300

    create mockService for authorization/registration

---
+++ iaw01/Services/NetworkService/AuthorizationNetworkService/AuthorizationNetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthorizationNetworkService.swift
+//  iaw01
+//
+//  Created by Дария Акатова on 21.05.2025.
+//
+
+import Foundation
---
+++ iaw01/Services/NetworkService/AuthorizationNetworkService/Dto/AuthorizationDto.swift
@@ -0,0 +1,8 @@
+//
+//  AuthorizationDto.swift
+//  iaw01
+//
+//  Created by Дария Акатова on 21.05.2025.
+//
+
+import Foundation
